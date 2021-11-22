9c5cadb1f4b213307b9929ae52b395e45d9ccc01
Pykel <Pykel@>
2021-11-22 20:44:23 +1000

    Minor logging tweak

--- src/stalk_logger.cpp
+++ src/stalk_logger.cpp
@@ -7,7 +7,7 @@ void Logger::log(Level lvl, const std::string& msg)
 {
     try
     {
-        const auto view = std::string_view(msg.data(), msg.size());// string_view_t(buf.data(), buf.size());
+        const auto view = std::string_view(msg.data(), msg.size());
 
         if (!logCb_)
         {
@@ -30,6 +30,26 @@ void Logger::log(Level lvl, const std::string& msg)
         else
             std::cerr << "Unknown exception in logger" << std::endl;
     }
+}
+
+Logger::Level Logger::levelFromString(const char* lvl)
+{
+    if (strcmp(lvl, "trace") == 0)
+        return Level::Trace;
+    if (strcmp(lvl, "debug") == 0)
+        return Level::Debug;
+    if (strcmp(lvl, "info") == 0)
+        return Level::Info;
+    if (strcmp(lvl, "warning") == 0)
+        return Level::Warn;
+    if (strcmp(lvl, "error") == 0)
+        return Level::Err;
+    if (strcmp(lvl, "critical") == 0)
+        return Level::Critical;
+    if (strcmp(lvl, "off") == 0)
+        return Level::Off;
+
+    return Level::Warn;
 }
 
 std::atomic<int> Logger::defaultLevel_ = Logger::Level::Info;
